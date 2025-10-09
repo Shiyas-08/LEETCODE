bd978fa009dcade32ecc73a8d4a90aef2ea0ec35
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-10-09 12:19:27 +0530

    Time: 0 ms (100%), Space: 54 MB (46.41%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+  let m=s.toLowerCase()
+return m    
+};
+let str = "HELLO WORLD!";
+console.log(toLowerCase(str));   
