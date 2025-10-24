e8a6e1941709a1648ce9e022cece103f3834be4b
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-10-24 14:10:50 +0530

    Time: 0 ms (100%), Space: 54.2 MB (58.84%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let w1=word1.join("")
+    let w2=word2.join("")
+    if(w1===w2){
+        return true
+    }else {
+        return false
+    }
+    // console.log(w1)
+    // console.log(w2)
+    
+    
+};
