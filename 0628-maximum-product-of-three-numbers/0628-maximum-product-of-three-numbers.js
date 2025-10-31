bce8428ed728648577a908c2f4793848a5c4c321
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-10-31 16:57:38 +0530

    Time: 35 ms (69.35%), Space: 58.1 MB (74.19%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    let sort=nums.sort((a,b)=>a-b)
+    let n=nums.length
+    return Math.max(
+        nums[n-1]*nums[n-2]*nums[n-3],
+         nums[0] * nums[1] * nums[n - 1]    
+        
+        )
+};
