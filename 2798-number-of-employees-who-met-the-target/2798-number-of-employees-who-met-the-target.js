eb61d670834e2b84736289d03300f4ba5573aff9
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-11-01 15:46:07 +0530

    Time: 0 ms (100%), Space: 57.3 MB (9.09%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+
+    let count=0;
+    for(let x of hours){
+        if(x>=target ){
+            count++
+        }
+
+    }
+    return count 
+    
+};
