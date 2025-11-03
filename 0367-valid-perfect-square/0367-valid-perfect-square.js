eafc9e5549ebb1df62b92cae27ad7050716feb74
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-11-03 22:23:56 +0530

    Time: 0 ms (100%), Space: 54.1 MB (52.58%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let n=Math.sqrt(num)
+    if(Number.isInteger(n)){
+        return true
+    }else{
+        return false
+    }
+
+    
+};
