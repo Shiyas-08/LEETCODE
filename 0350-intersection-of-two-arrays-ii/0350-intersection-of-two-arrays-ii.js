7842ae9fd2fcb2bae7f2f7e7fe3ee2c729268ed5
MUHAMMED SHIYAS C <shiyasmuhammed541@gmail.com>
2025-11-20 09:59:32 +0530

    Time: 5 ms (16.74%), Space: 56.8 MB (47%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    nums1.sort((a,b)=>a-b)
+    nums2.sort((a,b)=>a-b)
+    let arr=[]
+    let i=0;
+    let j=0
+    while(i<nums1.length && j<nums2.length){
+        if(nums1[i]<nums2[j])i++
+        else if(nums2[j]<nums1[i]) j++
+        else{
+            arr.push(nums1[i])
+            i++
+            j++
+        }
+
+    }
+    return arr
+  
+    
+    
+};
